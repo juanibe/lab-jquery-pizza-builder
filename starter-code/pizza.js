fa250c60a8a7297773401390884fc44ffacab3e4
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2018-10-30 22:07:08 +0100

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,13 +1,19 @@
 // Write your Pizza Builder JavaScript in this file.
+
+var totalPrice = 13;
+
 $(".btn-pepperonni").click(function(){
     $(".pep").toggle()
     if($(".pep:visible").length===0){
         $(".btn-pepperonni").removeClass("active")
         $("aside li:nth-child(1)").show()
+        totalPrice += 1
     }else{
         $(".btn-pepperonni").addClass("active")
         $("aside li:nth-child(1)").hide()
+        totalPrice -= 1
     }
+    $("aside strong").html("$"+totalPrice)
 })
 
 $(".btn-mushrooms").click(function(){
@@ -15,10 +21,13 @@ $(".btn-mushrooms").click(function(){
     if($(".mushroom:visible").length===0){
         $(".btn-mushrooms").removeClass("active")
         $("aside li:nth-child(2)").show()
+        totalPrice += 1
     }else{
         $(".btn-mushrooms").addClass("active")
         $("aside li:nth-child(2)").hide()
+        totalPrice -= 1
     }
+    $("aside strong").html("$"+totalPrice)
 })
 
 $(".btn-green-peppers").click(function(){
@@ -26,10 +35,13 @@ $(".btn-green-peppers").click(function(){
     if($(".green-pepper:visible").length===0){
         $(".btn-green-peppers").removeClass("active")
         $("aside li:nth-child(3)").show()
+        totalPrice += 1
     }else{
         $(".btn-green-peppers").addClass("active")
         $("aside li:nth-child(3)").hide()
+        totalPrice -= 1
     }
+    $("aside strong").html("$"+totalPrice)
 })
 
 $(".btn-crust").click(function(){
@@ -37,9 +49,11 @@ $(".btn-crust").click(function(){
     if($(".crust").hasClass("crust-gluten-free")){
         $(".btn-crust").removeClass("active")
         $("aside li:nth-child(5)").show()
+        totalPrice += 5
     }else{
         $(".btn-crust").addClass("active")
         $("aside li:nth-child(5)").hide()
+        totalPrice -= 5
     }
 })
 
@@ -50,10 +64,13 @@ $(".btn-sauce").click(function(){
    if($(".sauce").hasClass("sauce-white")){
     $(".btn-sauce").removeClass("active")
     $("aside li:nth-child(4)").show()
+    totalPrice += 3
 }else{
     $(".btn-sauce").addClass("active")
     $("aside li:nth-child(4)").hide()
+    totalPrice -= 3
 }
+$("aside strong").html("$"+totalPrice)
 })
 $()
 
@@ -61,4 +78,5 @@ $(".btn-crust").removeClass("crust-gluten-free")
 $(".sauce").removeClass("sauce-white")
 $(".btn-sauce").removeClass("active")
 $(".btn-crust").removeClass("active")
+
 
